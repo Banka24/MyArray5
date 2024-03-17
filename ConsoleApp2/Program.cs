ae6481a1f30e9d790b873f69f0b5f536b55bd73d
Banka24 <Aq567489012@gmail.com>
2024-03-17 20:12:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApp2;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.Write("Введите кол-во спортсменов: ");
+        if (!int.TryParse(Console.ReadLine(), out var countOfSportsmens))
+        {
+            Console.WriteLine("Введите число");
+            return;
+        }
+
+        var sportsmens = new Sportsmen[countOfSportsmens];
+        InputSportsmens(ref sportsmens);
+        PrintSwimmer(sportsmens);
+    }
+
+    private static void InputSportsmens(ref Sportsmen[] sportsmens)
+    {
+        for (var i = 0; i < sportsmens.Length; i++)
+        {
+            sportsmens[i] = new Sportsmen();
+            Console.Write("Введите фамилию спортсмена: ");
+            sportsmens[i].LastName = Console.ReadLine();
+            Console.Write("Введите имя спортсмена: ");
+            sportsmens[i].FirstName = Console.ReadLine();
+            Console.Write("Введите дату рождения: ");
+            sportsmens[i].BirthDate = DateTime.Parse(Console.ReadLine()).ToShortDateString();
+            Console.Write("Вид спорта: ");
+            sportsmens[i].SportName = Console.ReadLine();
+            Console.Write("Сколько лет занимается видом спорта: ");
+            sportsmens[i].YearInSport = uint.Parse(Console.ReadLine());
+        }
+    }
+
+    private static void PrintSwimmer(in Sportsmen[] sportsmens)
+    {
+        var sportsmenSelector = sportsmens.Where(i => i.SportName is "плавание" or "Плавание");
+
+        foreach (var sportsmen in sportsmenSelector)
+        {
+            Console.WriteLine($"Спортсмен {sportsmen.LastName} {sportsmen.FirstName}. Возраст: {DateTime.Today.Year - DateTime.Parse(sportsmen.BirthDate).Year}.\n" +
+                              $"Занимается спортом: {sportsmen.YearInSport}");
+        }
+            
+    }
+}
---
+++ ConsoleApp2/Sportsmen.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp2
+{
+    internal class Sportsmen
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string BirthDate { get; set; }
+        public string SportName { get; set; }
+        public uint YearInSport { get; set; }
+    }
+}
